46eb28af102dbb4132547bed8e756603e44a1b5d
VladimirDan <vovaoil2003@gmail.com>
2023-06-03 17:41:30 +0300

    Add unit test for health() func

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/jarcoal/httpmock"
 	. "gopkg.in/check.v1"
 )
 
-
 type MySuite struct{}
+
 var _ = Suite(&MySuite{})
 
 func TestBalancer(t *testing.T) { TestingT(t) }
@@ -46,4 +50,19 @@ func (s *MySuite) TestMin(c *C) {
 		{url: "Server3", connCnt: 10, healthy: true},
 	}
 	c.Assert(min(serversPool), Equals, 1)
-  }
+}
+
+func (s *MySuite) TestHealth(c *C) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	// Exact URL match
+	httpmock.RegisterResponder("GET", "http://api.mybiz.com/health",
+		httpmock.NewStringResponder(200, ""))
+	server := &Server{url: "api.mybiz.com"}
+
+	// do stuff that makes a request to articles
+	healthy := health(server)
+
+	c.Assert(healthy, Equals, true)
+}
